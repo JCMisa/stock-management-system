39fc65b8f3913de419ed6b1f76314c6b2781f3ca
MisaJC <johncarlomisa399@gmail.com>
2025-02-07 19:12:31 +0800

    remove file extension in path name of sitemap and robots

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,8 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
-  "/sitemap.ts",
-  "/robots.ts",
+  "/sitemap",
+  "/robots",
   "/",
 ]);
 
